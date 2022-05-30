6fc5214ffa5cd83814d1a512dc4bc6102e96528f
Sungoh Park <spark449062@gmail.com>
2022-05-30 06:45:46 -0600

    service logics (sign-up, find)

---
+++ src/main/java/spark/springpractice/service/MemberService.java
@@ -0,0 +1,36 @@
+package spark.springpractice.service;
+
+import spark.springpractice.domain.Member;
+import spark.springpractice.repository.MemberRepository;
+import spark.springpractice.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public long join(Member member) {
+
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("The username already exists");
+                });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
