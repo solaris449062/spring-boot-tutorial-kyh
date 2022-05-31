081e7a1913df2cf24b7f182a3033a5ef1f585546
Sungoh Park <spark449062@gmail.com>
2022-05-30 21:17:43 -0600

    manual dependency injection

--- src/main/java/spark/springpractice/controller/MemberController.java
+++ src/main/java/spark/springpractice/controller/MemberController.java
@@ -7,10 +7,15 @@ import spark.springpractice.service.MemberService;
 @Controller
 public class MemberController {
 
-    private final MemberService memberService;
+    private MemberService memberService;
 
     @Autowired
-    public MemberController(MemberService memberService) {
+    public void setMemberService(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    //    @Autowired
+//    public MemberController(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
 }
--- src/main/java/spark/springpractice/repository/MemoryMemberRepository.java
+++ src/main/java/spark/springpractice/repository/MemoryMemberRepository.java
@@ -5,7 +5,6 @@ import spark.springpractice.domain.Member;
 
 import java.util.*;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
