4cfc121d249b7e3dff625d1b742eb7d0ee618a26
Sungoh Park <spark449062@gmail.com>
2022-06-04 22:02:27 -0600

    Spring MVC example: account

---
+++ src/main/java/spark/springpractice/controller/HomeController.java
@@ -0,0 +1,14 @@
+package spark.springpractice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+
+        return "home";
+    }
+}
